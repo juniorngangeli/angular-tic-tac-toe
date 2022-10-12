245b0d0338e3f63374674b3ffb5e0a56e1787240
Junior Ngangeli <juniorngangeli3@gmail.com>
2022-10-12 21:19:42 +0100

    feat: add nebular for desing and update ui of board and square

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,9 +2,17 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: ` <button>{{ value }}</button> `,
-  styles: [],
+  template: `
+    <button *ngIf="!value" nbButton>{{ value }}</button>
+    <button *ngIf="value === 'X'" nbButton hero status="success">
+      {{ value }}
+    </button>
+    <button *ngIf="value === 'O'" nbButton hero status="info">
+      {{ value }}
+    </button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
 })
 export class SquareComponent {
-  @Input() value: 'X' | 'O';
+  @Input() value: 'X' | 'O' | '' = '';
 }
